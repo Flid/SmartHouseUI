7048f4b01e2d1579e161027ea663b3e4f45d0869
Anton Kirilenko <antony.kirilenko@gmail.com>
2015-10-27 20:04:39 +0000

    Changing weather icon

--- smart_house_ui/weather_widget.py
+++ smart_house_ui/weather_widget.py
@@ -1,3 +1,6 @@
+import re
+import os
+
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
@@ -9,6 +12,7 @@ AVERAGE_PRESSURE = 1013.25  # bar
 
 
 class WeatherWidget(Widget):
+    weather_icon = ObjectProperty(None)
     temp_out = ObjectProperty(None)
     temp_in = ObjectProperty(None)
     humidity_out = ObjectProperty(None)
@@ -16,10 +20,25 @@ class WeatherWidget(Widget):
     pressure = ObjectProperty(None)
     rain_forecast = ObjectProperty(None)
 
+    re_weather_icon = re.compile(r'^.*/(\d\d)[dn]\.png$')
+
     def __init__(self, *args, **kwargs):
         super(WeatherWidget, self).__init__(*args, **kwargs)
         Clock.schedule_once(self.update_weather, timeout=1)
         Clock.schedule_interval(self.update_weather, timeout=10)
+
+    def set_weather_icon_by_url(self, url):
+        result = self.re_weather_icon.match(url)
+
+        if not result:
+            print('Invalid weather url %s' % url)
+            return
+
+        name = 'static/weather/%s.png' % result.groups()[0]
+        if os.path.exists(name):
+            self.weather_icon.source = name
+        else:
+            print('Invalid weather url %s' % url)
 
     def set_state_error(self):
         self.temp_out.text = '--'
@@ -46,16 +65,18 @@ class WeatherWidget(Widget):
             self.set_state_error()
             return
 
-        self.temp_out.text = str(round(data['data']['temperature']))
-        self.humidity_out.text = str(round(data['data']['humidity']))
+        data = data['data']
 
-        delta_pressure = (data['data']['pressure'] - AVERAGE_PRESSURE) / data['data']['pressure'] * 100
+        self.temp_out.text = str(round(data['temperature']))
+        self.humidity_out.text = str(round(data['humidity']))
+
+        delta_pressure = (data['pressure'] - AVERAGE_PRESSURE) / data['pressure'] * 100
         self.pressure.text = 'Pressure: %s%0.1f %%' % (
             '+' if delta_pressure > 0 else '',
             delta_pressure,
         )
 
-        rain_forecast_rating = data['data']['rain_forecast_rating']
+        rain_forecast_rating = data['rain_forecast_rating']
         if rain_forecast_rating > 1.0:
             text = 'heavy rain'
         elif rain_forecast_rating > 0.5:
@@ -64,6 +85,8 @@ class WeatherWidget(Widget):
             text = 'drizzle'
         else:
             text = 'clear'
+
+        self.set_weather_icon_by_url(data['icon_url'])
 
         self.rain_forecast.text = 'Expected: %s (%0.2f)' % (text, rain_forecast_rating)
         self.set_state_ok()
