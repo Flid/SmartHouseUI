2e6dbbad5bc680b11e9cd9b593ff668ca0b6239c
Anton Kirilenko <anton.kirilenko@wazoku.com>
2016-01-12 18:33:51 +0000

    Game of Life screensaver

--- smart_house_ui/screens/idle.py
+++ smart_house_ui/screens/idle.py
@@ -2,14 +2,22 @@
 from __future__ import unicode_literals
 
 from datetime import datetime
+from random import random
+from itertools import product
 
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen, FadeTransition
 from kivy.uix.floatlayout import FloatLayout
 from kivy.logger import Logger as log
+from kivy.graphics import Color, Rectangle
 
 SCREENSAVER_TIMEOUT = 60
+FIRST_GENERATION_DENSITY = 0.3
+RESTART_EVERY = 180  # seconds
+LOOP_DELAY = 0.4
+
+CELL_SIZE = 16  # px
 
 
 class ScreensaverDrawingArea(FloatLayout):
@@ -19,11 +27,61 @@ class ScreensaverDrawingArea(FloatLayout):
 
     def restart(self):
         log.debug('Starting screensaver...')
-        Clock.schedule_interval(self._update, 1)
+
+        self._init()
+        Clock.schedule_interval(self._init, RESTART_EVERY)
         self._update()
+        Clock.schedule_interval(self._update, LOOP_DELAY)
+
+    def _init(self, *args):
+        self._cells_count_x = int(self.width / float(CELL_SIZE))
+        self._cells_count_y = int(self.height / float(CELL_SIZE))
+        self._cells = {}
+
+        self.canvas.before.clear()
+
+        with self.canvas.before:
+            for x in range(self._cells_count_x):
+                for y in range(self._cells_count_y):
+                    alpha = 1 if random() < FIRST_GENERATION_DENSITY else 0
+                    self._cells[(x, y)] = Color(rgba=[0.3, 0.3, 0.3, alpha])
+                    Rectangle(
+                        size=[CELL_SIZE, CELL_SIZE],
+                        pos=[x * CELL_SIZE, y * CELL_SIZE],
+                    )
 
     def _update(self, *args):
+        to_change = {}
 
+        for x in range(self._cells_count_x):
+            for y in range(self._cells_count_y):
+                cells = product(
+                    [
+                        x - 1 if x > 0 else self._cells_count_x - 1,
+                        x,
+                        x + 1 if x < self._cells_count_x - 1 else 0,
+                    ],
+                    [
+                        y-1 if y > 0 else self._cells_count_y - 1,
+                        y,
+                        y + 1 if y < self._cells_count_y - 1 else 0,
+                    ],
+                )
+                neighbours_total_life = sum(self._cells[item].a for item in cells)
+
+                if self._cells[(x, y)].a:
+                    neighbours_total_life -= 1
+
+                    if neighbours_total_life not in [2, 3]:
+                        to_change[(x, y)] = 0
+                else:
+                    if neighbours_total_life == 3:
+                        to_change[(x, y)] = 1
+
+        for item, value in to_change.items():
+            self._cells[item].a = value
+
+        # Updating sensors
         if not self._tempr_label:
             panels = App.get_running_app().main_screen.ids['panels']
             self._tempr_label = panels.ids['weather'].ids['temp_out']
@@ -41,6 +99,7 @@ class ScreensaverDrawingArea(FloatLayout):
     def stop(self):
         log.debug('Stopping screensaver...')
         Clock.unschedule(self._update)
+        Clock.unschedule(self._init)
 
 
 def reschedule_screensaver():
